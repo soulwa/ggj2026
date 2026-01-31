5038d8ff19db0c48729015f5e056b76c9771ec7d
Sam Lyon <sam.c.lyon@gmail.com>
2026-01-31 12:11:11 -0500

    globals

---
+++ Scripts/globals.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var death_count := 0
+var time_spent := 0.0
+
+var has_double_jump := false
+var has_downdash := false
+var has_crying := false
+
+# 0 to 1 to fill screen, "cutscene" maybe.
+# when its 1.0 you can swim
+var waterworld := 0.0
