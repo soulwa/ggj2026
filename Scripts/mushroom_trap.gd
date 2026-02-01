88e8436358c75f8e8106c0ef249bc269fc1fc7cb
SnowdustDev <196397083+buzaii@users.noreply.github.com>
2026-02-01 13:13:41 -0500

    basic mushroom spores integration

--- Scripts/mushroom_trap.gd
+++ Scripts/mushroom_trap.gd
@@ -4,9 +4,12 @@ class_name MushroomTrap extends Node2D
 
 
 var mushroom_guy_scene: PackedScene = preload("res://Scenes/mushroom_guy.tscn")
+var mushroom_spores_scene: PackedScene = preload("res://Scenes/mushroom_spores.tscn")
 
 var min_guys: int = 5
 var max_guys: int = 7
+
+var spore_delay: float = 0.3  ## Delay between spore burst and mushroom spawn
 
 var activated: bool = false
 
@@ -18,6 +21,17 @@ func _on_player_detect_area_body_entered(body: Node2D) -> void:
 func activate() -> void:
 	if !activated:
 		activated = true
+		
+		# Emit spore burst effect before spawning mushrooms
+		# Add to parent so it doesn't get hidden when the trap hides
+		var spores: MushroomSpores = mushroom_spores_scene.instantiate()
+		get_parent().add_child(spores)
+		spores.emit_burst(guy_spawn_point.global_position, Vector2.UP)
+		
+		# Wait for spore effect before spawning mushrooms
+		# await get_tree().create_timer(spore_delay).timeout
+		
+		# Spawn the mushroom guys
 		for i in randi_range(min_guys, max_guys):
 			var new_guy = mushroom_guy_scene.instantiate()
 			new_guy.global_position = guy_spawn_point.global_position
